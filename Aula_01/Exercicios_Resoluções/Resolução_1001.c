016218d4d1dbdd57618b4f51e8d339bf0a477a23
Leppyzin <130197468+Leppyzin@users.noreply.github.com>
2026-04-08 15:03:22 -0300

    Add basic integer addition program
    Implement basic integer addition in C

---
+++ Aula_01/Exercicios_Resoluções/Resolução_1001.c
@@ -0,0 +1,15 @@
+//O problema 1 é bem básico, ele utiliza uma variável do tipo inteiro e utiliza a soma de dois inteiros, execute isso e parábens!! resolveu o problema!
+
+#include <stdio.h>
+
+int main(){ //estrutura basica da linguagem C
+    int x,y,r; //declarando minhas variáveis do tipo inteiro
+
+    scanf("%d%d",&x,&y); //recebendo os valores variáveis do meu usúario.
+  
+    r = x+y; //aqui estou atribuindo "==" um valor da soma dos 2 termos para a minha variável
+
+    printf("X = %d\n",r); // aqui estou apenas printando meu resultado no caso a soma das minhas duas variáveis
+
+    return 0; //confirma que está tudo bem com o programa.
+}
